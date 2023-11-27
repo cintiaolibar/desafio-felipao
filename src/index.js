db5f1a92c1411f3be5d61c0a65b27d3c304f22c7
Cintia <cintia.olibar@gmail.com>
2023-11-27 18:52:56 -0300

    Desafio do Felipão: Classificador de Nível de Herói

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 
-
-
-let xp = 3.001;
+let xp = 13.000;
 let nivelDoHeroi;
 let nome = "Vortex";
-
-
 
 
 if (xp <= 1.000) {
@@ -13,26 +9,20 @@ if (xp <= 1.000) {
 
   } else if (xp >= 1.001 && xp <= 2.000){
     nivelDoHeroi = "Bronze";
-
   } else if (xp >= 2.001 && xp <= 5.000){
     nivelDoHeroi = "Prata";
-
   } else if (xp >= 6.001 && xp <= 7.000){
     nivelDoHeroi = "Ouro";
-
   } else if (xp >= 7.001 && xp <= 8.000){
     nivelDoHeroi = "Platina";
-
-  } else if (xp >= 8.000 && xp <= 9.000){
+  } else if (xp >= 8.001 && xp <= 9.000){
     nivelDoHeroi = "Ascendente";
-
   } else if (xp >= 9.001 && xp <= 10.000){
-    nivelDoHeori = "Imortal"
-
+    nivelDoHeroi = "Imortal"
   } else if (xp >= 10.001){
     nivelDoHeroi = "Radiante"
-
   } 
 
+console.log("O Herói de nome " + nome + "está no nível " + nivelDoHeroi);
 
-console.log("O Herói de nome Vortex está no nível " + nivelDoHeroi);
+
