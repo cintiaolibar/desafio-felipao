60d5ef9fc22e530888973aebbb1c228b81e302fc
Cintia <cintia.olibar@gmail.com>
2023-11-27 18:37:49 -0300

    Desafio: Classificador de Nível de Herói

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 
-let nome = "felipe";
-console.log(nome); 
+
+
+let xp = 3.001;
+let nivelDoHeroi;
+let nome = "Vortex";
+
+
+
+
+if (xp <= 1.000) {
+    nivelDoHeroi = "Ferro";
+
+  } else if (xp >= 1.001 && xp <= 2.000){
+    nivelDoHeroi = "Bronze";
+
+  } else if (xp >= 2.001 && xp <= 5.000){
+    nivelDoHeroi = "Prata";
+
+  } else if (xp >= 6.001 && xp <= 7.000){
+    nivelDoHeroi = "Ouro";
+
+  } else if (xp >= 7.001 && xp <= 8.000){
+    nivelDoHeroi = "Platina";
+
+  } else if (xp >= 8.000 && xp <= 9.000){
+    nivelDoHeroi = "Ascendente";
+
+  } else if (xp >= 9.001 && xp <= 10.000){
+    nivelDoHeori = "Imortal"
+
+  } else if (xp >= 10.001){
+    nivelDoHeroi = "Radiante"
+
+  } 
+
+
+console.log("O Herói de nome Vortex está no nível " + nivelDoHeroi);
